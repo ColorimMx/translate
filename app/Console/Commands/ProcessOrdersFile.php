fd3391631bff59559ec246c24c0c44638eddc33e
pablinux88 <pablo.amador88@gmail.com>
2024-12-16 17:05:38 -0600

    Comando Nadro
    Se crea el comando que procesa las ordenes de Nadro para traducirlas en 850_EXP.CIM

--- app/Console/Commands/ProcessOrdersFile.php
+++ app/Console/Commands/ProcessOrdersFile.php
@@ -75,7 +75,7 @@ class ProcessOrdersFile extends Command
 
             //Storage::disk('edi')->move("translate/{$fileName}", "translate_process/{$fileName}");
             //$logData[] = $this->generateLogMessage("Processed successfully: {$fileName} - Moved to translate_process - File has been generated 850_EXP.CIM in data_in.");
-            $this->logToFile($logFilePath, $logData);
+            //$this->logToFile($logFilePath, $logData);
         }
 
         return Command::SUCCESS;
