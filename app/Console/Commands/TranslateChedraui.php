f8a7f3f0600abd911ef729a99b8ec0eb277f0181
pablinux88 <pablo.amador88@gmail.com>
2024-12-19 16:09:32 -0600

    Agrupación artículos Comando Chedraui
    Se identifico en la sesión de carga de la orden de Chedraui del día 19/12/24 que las ordenes INF, están desglosadas a detalle por tienda, se modifica el código para que las traduzca Orden/Articulo

--- app/Console/Commands/TranslateChedraui.php
+++ app/Console/Commands/TranslateChedraui.php
@@ -34,8 +34,10 @@ class TranslateChedraui extends Command
         $typeTransLine = '300';
         $tpCode = 'T1250';
         $trxCode = '850';
-        $previousOrder = null;
 
+        $groupedItems = [];
+
+        // Agrupar artículos y totalizar cantidades multiplicadas
         foreach (array_slice($lines, 1) as $line) {
             $columns = str_getcsv($line);
 
@@ -45,48 +47,65 @@ class TranslateChedraui extends Command
             }
 
             $currentOrder = $columns[0];
+            $articleCode = $columns[5];
+            $quantity = $columns[8] * $columns[14]; // Realizar la multiplicación
 
-            if ($currentOrder !== $previousOrder) {
-                $line1 =
-                    str_pad(substr($customer, 0, 2), 2) .
-                    str_pad(substr($currentOrder, 0, 22), 22) .
-                    str_pad(substr($columns[2], 0, 8), 8) .
-                    str_pad('', 7) .
-                    str_pad(substr($typeTrans, 0, 3), 3) .
-                    str_pad('', 11) .
-                    str_pad(substr($tpCode, 0, 5), 5) .
-                    str_pad('', 15) .
-                    str_pad(substr($trxCode, 0, 3), 3) .
-                    PHP_EOL;
-
-                $outputData[] = $line1;
-                $previousOrder = $currentOrder;
+            if (!isset($groupedItems[$currentOrder])) {
+                $groupedItems[$currentOrder] = [];
             }
 
-            $priceFormatted = $this->formatPrice($columns[10]/$columns[8]);
+            if (!isset($groupedItems[$currentOrder][$articleCode])) {
+                $groupedItems[$currentOrder][$articleCode] = [
+                    'columns' => $columns,
+                    'totalQuantity' => 0
+                ];
+            }
 
-            $line2 =
+            $groupedItems[$currentOrder][$articleCode]['totalQuantity'] += $quantity; // Totalizar
+        }
+
+        // Generar líneas de salida
+        foreach ($groupedItems as $order => $articles) {
+            $line1 =
                 str_pad(substr($customer, 0, 2), 2) .
-                str_pad(substr($currentOrder, 0, 22), 22) .
-                str_pad('', 9) .
-                str_pad('', 6) .
-                str_pad(substr($typeTransLine, 0, 3), 3) .
+                str_pad(substr($order, 0, 22), 22) .
+                str_pad(substr(reset($articles)['columns'][2], 0, 8), 8) .
+                str_pad('', 7) .
+                str_pad(substr($typeTrans, 0, 3), 3) .
                 str_pad('', 11) .
                 str_pad(substr($tpCode, 0, 5), 5) .
-                str_pad('', 85) .
-                str_pad(substr('00000', 0, 15), 15) .
-                str_pad('', 31) .
-                str_pad(substr($columns[5], 0, 30), 30) .
-                str_pad('', 30) .
-                str_pad(str_pad($columns[8]*$columns[14], 9, '0', STR_PAD_LEFT), 9) .
-                str_pad(substr('PZ', 0, 2), 2) .
-                $priceFormatted .
-                str_pad('', 2) .
-                str_pad('', 68) .
-                str_pad(substr($columns[3], 0, 8), 8) .
+                str_pad('', 15) .
+                str_pad(substr($trxCode, 0, 3), 3) .
                 PHP_EOL;
 
-            $outputData[] = $line2;
+            $outputData[] = $line1;
+
+            foreach ($articles as $articleCode => $data) {
+                $priceFormatted = $this->formatPrice($data['columns'][10] / $data['columns'][8]);
+
+                $line2 =
+                    str_pad(substr($customer, 0, 2), 2) .
+                    str_pad(substr($order, 0, 22), 22) .
+                    str_pad('', 9) .
+                    str_pad('', 6) .
+                    str_pad(substr($typeTransLine, 0, 3), 3) .
+                    str_pad('', 11) .
+                    str_pad(substr($tpCode, 0, 5), 5) .
+                    str_pad('', 85) .
+                    str_pad(substr('00000', 0, 15), 15) .
+                    str_pad('', 31) .
+                    str_pad(substr($articleCode, 0, 30), 30) .
+                    str_pad('', 30) .
+                    str_pad(str_pad($data['totalQuantity'], 9, '0', STR_PAD_LEFT), 9) . // Cantidad totalizada
+                    str_pad(substr('PZ', 0, 2), 2) .
+                    $priceFormatted .
+                    str_pad('', 2) .
+                    str_pad('', 68) .
+                    str_pad(substr($data['columns'][3], 0, 8), 8) .
+                    PHP_EOL;
+
+                $outputData[] = $line2;
+            }
         }
 
         Storage::disk('edi')->put($outputFilePath, implode('', $outputData));
